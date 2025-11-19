8f078326a689c6a52076cf2bb2263e08d215dc7a
Warriorid <i.hockeyakk@fmail.com>
2025-11-19 16:27:37 +0300

    update ghpages

--- pit-calculation/src/pages/HomePages/HomePage.tsx
+++ pit-calculation/src/pages/HomePages/HomePage.tsx
@@ -2,7 +2,6 @@ import { FC } from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Nav, Container, Row, Col } from "react-bootstrap";
 import Header from "../../components/Header/Header";
-import { getStaticPath } from "../../utils/paths"; // Добавьте этот импорт
 import "./HomePage.css";
 
 export const HomePage: FC = () => {
@@ -20,18 +19,20 @@ export const HomePage: FC = () => {
       </Navbar>
       
       <div className="video-background">
-      <video 
-        autoPlay 
-        muted 
-        loop 
-        playsInline
-        disablePictureInPicture
-        controls={false}
-        className="background-video">
-        <source 
-          src={getStaticPath("/static/videos/construction-background.MP4")} type="video/mp4" />
+        <video 
+          autoPlay 
+          muted 
+          loop 
+          playsInline
+          disablePictureInPicture
+          controls={false}
+          className="background-video">
+          <source 
+            src="/Pits-calculation-frontend/pit-calculation/public/static/videos/construction-background.MP4" 
+            type="video/mp4" 
+          />
           Ваш браузер не поддерживает видео.
-      </video>
+        </video>
         
         <Container className="home-container">
           <Row>
